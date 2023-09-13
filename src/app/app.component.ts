0dfea7a71b16efb88cb3bdd0798a63f27c89c6f8
Mugisha-isaac <mugishaisaac0508@gmail.com>
2023-09-13 11:55:12 +0200

    Ft: interpolation and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageHeader: string = 'Employee Details';
-
+  imagePath: string = 'content/v1/5a09a654e5dd5bcfe00c5f3a/1592211840043-27ZDQFXXJZPBJHOBVWR4/Photo+10-06-2020%2C+6+36+58+PM.jpg?format=1500w';
   firstName: string = 'Tom';
   lastName: string = 'Hopkins';
+  isDisabled: boolean = true;
 
    getFullName(): string {
     return this.firstName + ' ' + this.lastName;
