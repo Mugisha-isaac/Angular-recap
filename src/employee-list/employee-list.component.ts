7fef16325b6aab184e8184e4c9a294cd1c097ff1
Mugisha-isaac <mugishaisaac0508@gmail.com>
2023-09-14 23:22:31 +0200

    Ft: add event for listening to changes

--- src/employee-list/employee-list.component.ts
+++ src/employee-list/employee-list.component.ts
@@ -14,11 +14,11 @@ selectedEmployeeRadioButtonCount: string = 'all';
 
 constructor(){
   this.employees= [
-    { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988' },
-    { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
-    { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
-    { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '10/14/1980' },
-    {code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826,dateOfBirth: '12/15/1982' }
+    { code: 'emp101', name: 'Tom', gender: 'male', annualSalary: 5500, dateOfBirth: '6/25/1988' },
+    { code: 'emp102', name: 'Alex', gender: 'male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
+    { code: 'emp103', name: 'Mike', gender: 'male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
+    { code: 'emp104', name: 'Mary', gender: 'female', annualSalary: 6500.826, dateOfBirth: '10/14/1980' },
+    {code: 'emp105', name: 'Nancy', gender: 'female', annualSalary: 6700.826,dateOfBirth: '12/15/1982' }
 ];
 }
 
@@ -27,17 +27,17 @@ getTotalEmployeesCount(): number{
 }
 
 getTotalMaleEmployees(): number{
-  const maleEmployees = this.employees.filter((employee:any)=> employee.gender === 'Male');
+  const maleEmployees = this.employees.filter((employee:any)=> employee.gender === 'male');
   return maleEmployees.length;
 }
 
 getTotalFemaleEmployees(): number{
-  const femaleEmployees = this.employees.filter((employee: any)=>employee.gender === 'Female')
+  const femaleEmployees = this.employees.filter((employee: any)=>employee.gender === 'female')
   return femaleEmployees.length;
 }
 
-onEmployeeCountRadioChange(selectedRadioButtonValue: string): void{
+onEmployeeCountRadioChange(selectedRadioButtonValue: any): void{
   this.selectedEmployeeRadioButtonCount = selectedRadioButtonValue;
+  console.log('selected value....', this.selectedEmployeeRadioButtonCount);
 }
-
 }
